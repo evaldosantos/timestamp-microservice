af428130e847953a2e84bea1b0ebd722dcec3214
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-05 17:18:07 +0200

    setup test

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+export default app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,18 @@
-import assert from 'assert';
-import main from '../src/index';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+
+import app from '../src/index';
+
+chai.use(chaiHttp);
 
 describe('test suit', function () {
-  it('asserts true', function () {
-    assert(main());
+  it('GET /', function () {
+    chai
+      .request(app)
+      .get('/')
+      .end(function (err, res) {
+        expect(res.text).to.eq('Hello World!');
+        expect(res).to.have.status(200);
+      });
   });
 });
