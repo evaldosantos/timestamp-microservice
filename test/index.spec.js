7b4d49cd07c40aba25951364834d9c8683b88f41
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-05 21:29:46 +0200

    override prettier/arrow-parens

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,31 +7,31 @@ chai.use(chaiHttp);
 
 describe('Timestamp microservice', () => {
   describe('GET /api/timestamp/:date_string?', () => {
-    let request;
-
-    beforeEach(function () {
-      request = chai.request(app).get('/api/timestamp/2015-12-25');
+    it('should handle a valid date, and return the correct unix timestamp', done => {
+      chai
+        .request(app)
+        .get('/api/timestamp/2015-12-25')
+        .end((err, res) => {
+          const response = { unix: 1451001600000 };
+          expect(res.body).to.include(response);
+          expect(res).to.have.status(200);
+          done();
+        });
     });
 
-    it('should handle a valid date, and return the correct unix timestamp', (done) => {
-      request.end((err, res) => {
-        const response = { unix: 1451001600000 };
-        expect(res.body).to.include(response);
-        expect(res).to.have.status(200);
-        done();
-      });
+    it('should handle a valid date, and return the correct UTC string', done => {
+      chai
+        .request(app)
+        .get('/api/timestamp/2015-12-25')
+        .end((err, res) => {
+          const response = { utc: 'Fri, 25 Dec 2015 00:00:00 GMT' };
+          expect(res.body).to.include(response);
+          expect(res).to.have.status(200);
+          done();
+        });
     });
 
-    it('should handle a valid date, and return the correct UTC string', (done) => {
-      request.end((err, res) => {
-        const response = { utc: 'Fri, 25 Dec 2015 00:00:00 GMT' };
-        expect(res.body).to.include(response);
-        expect(res).to.have.status(200);
-        done();
-      });
-    });
-
-    it('should handle a valid unix date, and return the correct unix timestamp', (done) => {
+    it('should handle a valid unix date, and return the correct unix timestamp', done => {
       chai
         .request(app)
         .get('/api/timestamp/1451001600000')
