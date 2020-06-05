ad5351dd27366943c38e40ef6a2322a7e8139698
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-05 21:00:23 +0200

    allow arrow function as callback in it/describe functions

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,15 +5,15 @@ import app from '../src/index';
 
 chai.use(chaiHttp);
 
-describe('Timestamp microservice', function () {
-  describe('GET /api/timestamp/:date_string?', function () {
+describe('Timestamp microservice', () => {
+  describe('GET /api/timestamp/:date_string?', () => {
     let request;
 
     beforeEach(function () {
       request = chai.request(app).get('/api/timestamp/2015-12-25');
     });
 
-    it('should handle a valid date, and return the correct unix timestamp', function (done) {
+    it('should handle a valid date, and return the correct unix timestamp', (done) => {
       request.end((err, res) => {
         const response = { unix: 1451001600000 };
         expect(res.body).to.include(response);
@@ -22,7 +22,7 @@ describe('Timestamp microservice', function () {
       });
     });
 
-    it('should handle a valid date, and return the correct UTC string', function (done) {
+    it('should handle a valid date, and return the correct UTC string', (done) => {
       request.end((err, res) => {
         const response = { utc: 'Fri, 25 Dec 2015 00:00:00 GMT' };
         expect(res.body).to.include(response);
