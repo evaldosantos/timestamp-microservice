c57b92a3ba2cdef18c1543d7aa6324b325b1ec15
Evaldo Santos <jesantosjr@protonmail.com>
2020-06-05 17:09:55 +0200

    add expressjs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { defaultValue } from './module';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-export default function () {
-  return defaultValue;
-}
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
